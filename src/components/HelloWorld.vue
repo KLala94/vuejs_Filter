deccb239fc7114114af580a66d0821b7af215602
Lala <lalaklod@gmail.com>
2019-11-28 15:50:41 +0100

    filtered array by color

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,20 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="HelloWorld">
+       <button class="allButton" v-on:click='changeColor("")'>all</button>
+         <button class="redButton" v-on:click='changeColor("red")'>Red</button>
+         <button class="blueButton" v-on:click='changeColor("blue")'>Blue</button>
+         <button class="yellowButton" v-on:click='changeColor("yellow")'>Yellow</button>
+         <button class="purpleButton" v-on:click='changeColor("purple")'>Purple</button>
+    <div v-for="item in list" 
+    :key="item.id">
+    <div class="items" v-if="ifColor == '' || item.color == ifColor ">
+    <p>The color is: {{item.color}}</p>
+    <p>The price is: {{item.price}}</p>
+    </div>
+   
+    </div>
+  </div>
   </div>
 </template>
 
@@ -34,25 +22,77 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+  },
+  data: function(){
+    return{
+      list: [
+    {    id : 1,
+        color : "red",
+        price : 500
+    },
+    {    id : 2,
+        color : "green",
+        price : 200
+    },
+    {    id : 3,
+        color : "blue",
+        price : 700
+    },{
+        id : 4,
+        color : "yellow",
+        price : 800
+    },
+    {    id : 5,
+        color : "purple",
+        price : 1000
+    }],
+    ifColor : ''
+    }
+  },
+  methods:{
+    changeColor(e){
+    return this.ifColor = e
+      
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.items{
+   display: inline-block;
+    width:300px;
+    height: 300px;
+    background-color: cadetblue;
+    margin: 10px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.redButton{
+    background-color: lightcoral;
+    color: rgb(124, 3, 3);
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.blueButton{
+    background-color:lightskyblue;
+    color:blue;
 }
-a {
-  color: #42b983;
+.yellowButton{
+    background-color: burlywood;
+    color: yellow;
+}
+.purpleButton{
+background-color: rgb(182, 139, 204);
+color: blueviolet;
+}
+.greenButton{
+background-color: chartreuse;
+color: green;
+}
+button{
+    border-radius: 45%;
+    width: 150px;
+    height: 70px;
+    float: left;
+    margin: 0px;
+   
 }
 </style>
