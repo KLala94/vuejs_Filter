60d6fa5de4085e5768ff9afacfe35cf64f561f8d
Lala <lalaklod@gmail.com>
2019-11-28 15:50:41 +0100

    filtered array by color

---
+++ src/assets/data.js
@@ -0,0 +1,22 @@
+const data = [
+    {    id : 1,
+        color : "red",
+        price : 500
+    },
+    {    id : 2,
+        color : "green",
+        price : 200
+    },
+    {    id : 3,
+        color : "blue",
+        price : 700
+    },{
+        id : 4,
+        color : "yellow",
+        price : 800
+    },
+    {    id : 5,
+        color : "purple",
+        price : 1000
+    }]
+    export default data
